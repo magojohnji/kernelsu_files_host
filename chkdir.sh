baf478fd3e76fba58bb1dc917e680b207042b257
magojohnji <magojohnji@outlook.com>
2023-08-20 18:28:17 +0800

    Improvement

---
+++ chkdir.sh
@@ -0,0 +1,15 @@
+if [ ! -d "./offical/" ];then
+ mkdir ./offical
+fi
+
+if [ ! -d "./offical/apk/" ];then
+ mkdir ./offical/apk
+fi
+
+if [ ! -d "./kernelsu_debug/" ];then
+ mkdir ./kernelsu_debug
+fi
+
+if [ ! -d "./kernelsu_debug/apk/" ];then
+ mkdir ./kernelsu_debug/apk
+fi
